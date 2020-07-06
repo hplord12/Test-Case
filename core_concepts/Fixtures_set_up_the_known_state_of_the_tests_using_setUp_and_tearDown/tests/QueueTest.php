bc62dfd3978a683586aa3c038660d8eabf7d3872
hardikpatel1990 <hardik.patel@qed42.com>
2020-07-06 16:27:40 +0530

    adding resource code for phpunit

---
+++ core_concepts/Fixtures_set_up_the_known_state_of_the_tests_using_setUp_and_tearDown/tests/QueueTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class QueueTest extends TestCase
+{
+    protected $queue;
+        
+    protected function setUp(): void
+    {
+        $this->queue = new Queue;        
+    }
+    
+    protected function tearDown(): void
+    {
+        unset($this->queue);
+    }
+        
+    public function testNewQueueIsEmpty()
+    {
+        $this->assertEquals(0, $this->queue->getCount());
+    }
+
+    public function testAnItemIsAddedToTheQueue()
+    {
+        $this->queue->push('green');
+        
+        $this->assertEquals(1, $this->queue->getCount());
+    }
+
+    public function testAnItemIsRemovedFromTheQueue()
+    {
+        $this->queue->push('green');
+                
+        $item = $this->queue->pop();
+        
+        $this->assertEquals(0, $this->queue->getCount());
+        
+        $this->assertEquals('green', $item);
+    }     
+}
